134017b2bcaef718af087b675b0a2dacfc3de2cf
ChrispyWugen <christian.warken@gmx.net>
2021-12-05 21:58:08 +0100

    fixed connection, first working version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,9 @@
  * @since 2021-Dez-05
  */
 
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
+import org.apache.commons.configuration2.interpol.ExprLookup;
+
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -25,50 +25,63 @@ public class Main {
         try {
 
             //create config file and read properties
-            System.out.println("Read Config");
+            System.out.println("APP: Read Config");
             Configuration config = Configuration.getInstance();
             config.readConfiguration();
-            System.out.println("Reading Config successful"+"\n");
+            System.out.println("APP: Reading Config successful"+"\n");
 
-            System.out.println("Found User: " + config.getUser());
-            System.out.println("Found Password: " + config.getPassword());
-            System.out.println("Found Content: " + config.getContent());
-            System.out.println("Found RecipientMail: " + config.getRecipientMail());
-            System.out.println("Found SenderMail: " + config.getSenderMail());
-            System.out.println("Found System: " + config.getSystem());
+            System.out.println("APP: Found User: " + config.getUser());
+            System.out.println("APP: Found Password: " + config.getPassword());
+            System.out.println("APP: Found Content: " + config.getContent());
+            System.out.println("APP: Found RecipientMail: " + config.getRecipientMail());
+            System.out.println("APP: Found SenderMail: " + config.getSenderMail());
+            System.out.println("APP: Found System: " + config.getSystem());
 
+            //set values from config and/or create necessary objects
+            final String curUser = config.getUser();
+            final String curPassword = config.getPassword();
+            InternetAddress addressFrom = new InternetAddress(config.getSenderMail());
+            InternetAddress addressTo = new InternetAddress(config.getRecipientMail());
 
             // Get system properties
             Properties properties = System.getProperties();
-
-            // Setup mail server
+            // Setup mail server data
             properties.setProperty("mail.smtp.host", config.getSystem());
-            properties.setProperty("mail.user", config.getUser());
-            properties.setProperty("mail.password", config.getPassword());
-            //properties.setProperty("mail.smtp.socketFactory.port","25"); TODO
-            //properties.setProperty("mail.smtp.port","465"); TODO
+            properties.put("mail.smtp.auth","true");
+            properties.put("mail.smtp.port","587");//587 for gmail
+            properties.put("mail.debug","true");
+            properties.put("mail.smtp.ssl.trust",config.getSystem());
+            properties.setProperty("mail.smtp.starttls.enable", "true");
+
+            //create authentication object
+            Authenticator auth = new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(curUser,curPassword);
+                }
+            };
 
             // Get the default Session object.
-            Session session = Session.getDefaultInstance(properties);
+            Session session = Session.getDefaultInstance(properties, auth);
+
+            //debug
+            //Transport transport = session.getTransport(); //debug1/3
+
 
             // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(config.getSenderMail()));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(config.getRecipientMail()));
-
-            // Set Subject: header field
+            //set header fields
+            message.setSender(addressFrom);
+            message.addRecipient(Message.RecipientType.TO, addressTo);
             message.setSubject("This is the Subject Line!");
-
             // Now set the actual message
-            message.setText("This is actual message");
+            message.setContent("This is actual message", "text/plain");
 
             // Send message
+            //transport.connect(); //debug2/3
             Transport.send(message);
-            System.out.println("Sent message successfully....");
+            //transport.close(); //debug3/3
+            System.out.println("APP: Sent message successfully....");
 
         } catch (Exception e) {
             e.printStackTrace();
