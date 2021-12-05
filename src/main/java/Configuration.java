f24130c11c28f046082076679dc70dc155e636cc
ChrispyWugen <christian.warken@gmx.net>
2021-12-05 12:43:23 +0100

    implemented configuration

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,85 @@
+/**
+ * An implementation of Configuration
+ * in CW_SendMail
+ *
+ * @author chris
+ * @version 1.0
+ * @since 2021-Dez-05
+ */
+
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+
+    /* ---------------------------------------- Main ---------------------------------------------------------------- */
+
+
+
+    /* ---------------------------------------- Attributes ---------------------------------------------------------- */
+
+    private String user = "";
+    private String password = "";
+    private String content = "";
+    private static Configuration config = null; //singleton
+
+    /* ---------------------------------------- Constants ----------------------------------------------------------- */
+
+
+
+    /* ---------------------------------------- Constructors -------------------------------------------------------- */
+    /**
+     * Class holds the application configuration from properties file
+     */
+    private Configuration() {
+    }
+
+    public static synchronized Configuration getInstance() {
+        if (config == null) {
+            config = new Configuration();
+        }
+        return config;
+    }
+
+
+    /* ---------------------------------------- Methods ------------------------------------------------------------- */
+
+    /**
+     * Reads a PROPERTIES file and stores the data in a new instance of this class
+     *
+     * @throws IOException - if no PROPERTIES file can be read
+     */
+    public void readConfiguration(){
+        Properties properties = new Properties();
+        BufferedInputStream stream;
+        try{
+            stream = new BufferedInputStream(new FileInputStream("config.properties"));
+            properties.load(stream);
+            stream.close();
+            this.user = properties.getProperty("user");
+            this.password = properties.getProperty("password");
+            this.content = properties.getProperty("content");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /* ---------------------------------------- S/Getters ----------------------------------------------------------- */
+
+    public java.lang.String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    /* ---------------------------------------- toString ----------------------------------------------------------- */
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,33 @@
  * @version 1.0
  * @since 2021-Dez-05
  */
+
 public class Main {
 
     /* ---------------------------------------- Main ---------------------------------------------------------------- */
 
 
+
+    public static void main(String[] args) {
+
+        try {
+
+            //create config file and read properties
+            System.out.println("Read Config");
+            Configuration config = Configuration.getInstance();
+            config.readConfiguration();
+            System.out.println("Reading Config successful"+"\n");
+
+            System.out.println("Found User: " + config.getUser());
+            System.out.println("Found Password: " + config.getPassword());
+            System.out.println("Found Content: " + config.getContent());
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /* ---------------------------------------- Attributes ---------------------------------------------------------- */
 
